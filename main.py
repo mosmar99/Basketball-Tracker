864d29d64e91086fc07c1004b3fcec7c66216999
Mahmut Osmanovic <mahmut.osmanovic@outlook.com>
2025-11-09 11:31:23 +0100

    baseline team tracker

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from utils import read_video, save_video
 from tracking import PlayerTracker, BallTracker
 from canvas import PlayerTrackDrawer, BallTrackDrawer
+from team_assigner import TeamAssigner
 
 def main():
     # read video
-    vid_name = "video_1"
+    vid_name = "video_3"
     vid_frames = read_video(f"input_videos/{vid_name}.mp4")
 
     # init tracker
@@ -12,13 +13,17 @@ def main():
     player_tracker = PlayerTracker(model_path=model_path)
     ball_tracker = BallTracker(model_path=model_path)
 
-    # get tracks from videoframes
+    # get tracks from videoframes by team
     player_tracks = player_tracker.get_object_tracks(vid_frames,
                                                      read_from_stub=True,
                                                      stub_path="stubs/player_track_stubs.pkl")
     ball_tracks = ball_tracker.get_object_tracks(vid_frames,
-                                                   read_from_stub=True,
-                                                   stub_path="stubs/ball_track_stubs.pkl")
+                                                 read_from_stub=True,
+                                                 stub_path="stubs/ball_track_stubs.pkl")
+    team_assigner = TeamAssigner(team_A="RED shirt", team_B="WHITE shirt")
+    team_player_assignments = team_assigner.get_player_teams_over_frames(vid_frames, player_tracks, 
+                                                                         read_from_stub=True, 
+                                                                         stub_path="stubs/player_assignment_stubs.pkl")
     
     # erase wrongly detected basketball tracks & interp. between conservative basketball positions
     ball_tracks = ball_tracker.remove_incorrect_detections(ball_tracks)
@@ -26,7 +31,9 @@ def main():
 
     # fill canvas with annotations
     player_tracks_drawer = PlayerTrackDrawer()
-    player_vid_frames = player_tracks_drawer.draw_annotations(vid_frames, player_tracks)
+    player_vid_frames = player_tracks_drawer.draw_annotations(vid_frames, 
+                                                              player_tracks, 
+                                                              team_player_assignments)
 
     ball_track_drawer = BallTrackDrawer()
     output_vid_frames = ball_track_drawer.draw_annotations(player_vid_frames, ball_tracks)
