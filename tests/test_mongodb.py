04d6c2374af60896e6d83af41d3cc380cfcc1e49
Mahmut Osmanovic <mahmut.osmanovic@outlook.com>
2025-11-30 15:44:16 +0100

    successfully pinged mongodb cluster

---
+++ tests/test_mongodb.py
@@ -0,0 +1,21 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+username = os.getenv('USERNAME_MONGODB')
+password = os.getenv('PASSWORD_MONGODB')
+
+
+uri = f"mongodb+srv://{username}:{password}@basketball-tracker.j0hhg2r.mongodb.net/?appName=Basketball-Tracker"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
